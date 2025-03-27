e30563ab7035fffb322eb443541feb0745f8c82b
L-F-S <lorenzos@mail.tau.ac.il>
2025-03-27 11:38:07 +0100

    rearranged paths

--- src/modules/conf.py
+++ src/modules/conf.py
@@ -10,14 +10,28 @@ import os
 import pickle
 import pandas as pd
 
+####################
+# FLAGS
+####################
+ 
+# Disease symbol. This will be used in the pipeline to 
+# identify the disease. It will be in file names and directories
+# !! IMPORTANT: keep spaces around '=' and spaces before cmments for pipeline to work  
+DISEASE = 'ipf'  # 'als_NYGC' #
+
+
 ################
 # DIRECTORIES
 ################
 
-BASE_DIR=os.sep+'home'+os.sep+'netcos'+os.sep+'modules'+os.sep
+# BASE_DIR=os.sep+'home'+os.sep+'netcos'+os.sep+'modules'+os.sep
 
+DATA_DIR=BASE_DIR+'data'+os.sep
+DICT_DIR=DATA_DIR+'dictionaries'+os.sep
 
-MITH_DIR=BASE_DIR+'MITHRIL'+os.sep
+MITH_APP="/home/signorini/mithril3/app-3.0.0-SNAPSHOT.jar"
+
+MITH_DIR=BASE_DIR+'MITHrIL'+os.sep
 MITH_OUT=MITH_DIR+'output'+os.sep
 MITH_OUT_DISEASE=MITH_DIR+'output'+os.sep+'disease_signature'+os.sep
 MITH_OUT_DRUG=MITH_DIR+'output'+os.sep+'drug_signature'+os.sep
@@ -29,25 +43,23 @@ MITH_IN_DRUG=MITH_DIR+'input'+os.sep+'drug_signature'+os.sep
 
 TSR_DIR=BASE_DIR+'tsr'+os.sep
 TSR_OUT=TSR_DIR+'output'+os.sep
-TSR_OUT_DISEASE=TSR_OUT+'disease_signature'+os.sep
+TSR_OUT_DISEASE=TSR_OUT+'disease_signature'+os.sep+DISEASE+os.sep
 TSR_OUT_DRUG=TSR_OUT+'drug_signature'+os.sep
 TSR_OUT_DRUG_META=TSR_OUT_DRUG+'LINCS_lorenzo'+os.sep+'metanalysis_mith3_drug_wise'+os.sep
 TSR_OUT_CSCORE=TSR_OUT+'connectivity_score'+os.sep
 
 # Joined model dirs
 IMG_DIR=BASE_DIR+'imgs'+os.sep
-JM_OUT=BASE_DIR+'output'+os.sep
-CS_OUT=JM_OUT+'connectivity_score'+os.sep
+CS_DIR=BASE_DIR+'connectivity_score'+os.sep
+CS_IN_DRUG=CS_DIR+'input'+os.sep+'drug_signature'+os.sep
+CS_IN_DISEASE=CS_DIR+'input'+os.sep+'disease_signature'+os.sep+DISEASE+os.sep
+CS_OUT=CS_DIR+'output'+os.sep
 
 # landmark genes file:
     
 landmark_genes_filepath=TSR_DIR+'data'+os.sep+'LINCS-GSE92742'+os.sep+'landmark_genes.csv'
 
-####################
-# FLAGS
-####################
 
-DISEASE='ipf'#'als_NYGC'  #f  
 
 ####################
 # Utility functions
@@ -57,7 +69,7 @@ DISEASE='ipf'#'als_NYGC'  #f
 # more symbols may map to the same gene id
 
 # using signorini's mapping (see network_signing algorithm)
-alias_2geneid_filename=BASE_DIR+'utils'+os.sep+'alias_2geneid.pkl'
+alias_2geneid_filename=DICT_DIR+'alias_2geneid.pkl'
 with open(alias_2geneid_filename, 'rb') as f:
     alias_2geneid = pickle.load(f)
 
