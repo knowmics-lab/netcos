a43f771d222596d44e25e3fe1c20d7a3ab6cd3d1
L-F-S <lorenzos@mail.tau.ac.il>
2025-04-11 16:59:46 +0200

    Update X_bugfix_check_name_diff_mith_in_out.py

--- src/modules/X_bugfix_check_name_diff_mith_in_out.py
+++ src/modules/X_bugfix_check_name_diff_mith_in_out.py
@@ -5,22 +5,21 @@ Created on Thu Jan 30 16:54:33 2025
 
 @ author: L-F-S
 
+MITHrIL re-writes all file names that contain characters which are not
+alphanumeric, '_', or '-', for safety reasons.
+Run this to recuperate original file names, if needed.
 Renames mith3 output files by recovering oringinal characters from input files
 """
 import os
 if not os.getcwd().endswith('modules'):
     os.chdir('modules')
-import sys
-import numpy as np
-import pandas as pd
 import pickle
 import re
-from conf import DISEASE,MITH_IN_DRUG, MITH_OUT_DRUG,\
- TSR_OUT_DRUG, TSR_OUT_DISEASE, TSR_OUT_CSCORE, alias_2geneid
+from conf import MITH_IN_DRUG, MITH_OUT_DRUG
 
 
 #%% check diff between mith3 input and putout filenames
-print('mith3 batch output filename bug: osme filenames with special characters\
+print('mith3 batch output filename bug: some filenames with special characters\
       differ from input to output')
 
 mith_in_drug_names=set([n.split('.')[0] for n in os.listdir(MITH_IN_DRUG)])
@@ -33,7 +32,7 @@ def mithril_string_replace(input_string):
     
     return replaced_string
 
-print('does not work with this drug, but ok', mithril_string_replace('5\'-guanidinonaltrindole_24h'))
+# print('does not work with this drug, but ok', mithril_string_replace('5\'-guanidinonaltrindole_24h'))
 
 bad_to_good={mithril_string_replace(good):good for good in mith_in_drug_names}
 
