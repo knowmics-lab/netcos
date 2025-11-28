f50908fd01ac97f1c16e14d6ec847dcceed782ad
L-F-S <lorenzo.fsignorini@outlook.it>
2025-11-28 09:11:23 +0100

    chembl tsr in to mith out

---
+++ src/validations/chembl/tsr_out_to_mith_in.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 28 09:08:11 2025
+
+@author: los4
+"""
+import pandas as pd
+import os
+import pyreadr
+from conf import TSR_OUT_DRUG, cell_line, pert_time,\
+    MITH_IN_DRUG
+DRUG_DIR = TSR_OUT_DRUG+'LINCS'+os.sep+cell_line+'_'+pert_time+'_drug_wise'+os.sep
+df_list=[]
+for i, drug_file in enumerate(os.listdir(DRUG_DIR)):
+    drug = drug_file.rstrip('.Rds')
+    result = pyreadr.read_r(DRUG_DIR+drug_file)
+    print(drug)
+    df = result[None] 
+    df.set_index('gene_id', inplace=True)
+    df.rename(columns = {'DE_log2_FC':drug}, inplace = True)
+    
+    df_list.append(df[drug])
+
+matrix=pd.concat(df_list, axis=1)
+print(matrix.shape)
+print(matrix.columns)
+print(matrix.head(3))
+
+# Save MITHrIL gene id based input file:
+mith_input_filename = 'LINCS_'+cell_line+'_'+pert_time+'.mi'
+matrix.to_csv(MITH_IN_DRUG+mith_input_filename, sep='\t', index=True,  index_label=False)
+print('MITHrIL drug input saved in',MITH_IN_DRUG,'filename:',mith_input_filename)
