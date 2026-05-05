b2fb670638765f27d6e1fc9f7a7430494fc9cb00
L-F-S <lorenzo.fsignorini@outlook.it>
2026-05-05 13:10:51 +0200

    asdasdasd
    Signed-off-by: L-F-S <lorenzo.fsignorini@outlook.it>

--- src/conf.py
+++ src/conf.py
@@ -145,7 +145,7 @@ mith_threads = 10
 ###############################################################################
 
 # hyperparameters
-cs_batch_threads = 1
+cs_batch_threads = 30
 cs_mith = 1 # default 1: calculate on MITHrIL data, 0: calculate on DEG data
 cs_on_LM = 0 # possible values: [0,1] 0: calculate cs on all genes list 1: calculate on only landmark genes list
 CS_METHOD ="bin_chen_disease_sorted"
